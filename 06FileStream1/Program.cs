5823e07c41c1adeaa45a656cf18b42d7a6cd232a
Paulo Cesar Passos Pereira <paulo.passos.pereira1@outlook.com>
2025-01-06 15:01:32 -0300

    basico de FileStream

---
+++ 06FileStream1/Program.cs
@@ -0,0 +1,33 @@
+﻿string caminhoArquivo = @"C:\Users\paulo\Documents\dados\MeuDiretorio2\poesia.txt";
+
+// FileStream fs = null;
+StreamReader leitor = null;
+
+try
+{
+    // fs = new FileStream(caminhoArquivo, FileMode.Open, FileAccess.Read);
+    // leitor = new StreamReader(fs);
+    leitor = File.OpenText(caminhoArquivo);
+
+    string? linha;
+    while((linha = leitor.ReadLine()) != null)
+    {
+        Console.WriteLine(linha);
+    }
+
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+finally
+{
+    if(leitor != null) leitor.Close();
+    //if (fs != null) fs.Close();
+}
+
+Console.ReadKey();
---
+++ 07FileStreamBlocoUsing/Program.cs
@@ -0,0 +1,23 @@
+﻿string caminhoArquivo = @"C:\Users\paulo\Documents\dados\MeuDiretorio2\poesia.txt";
+
+try
+{
+    using FileStream fs = new FileStream(caminhoArquivo, FileMode.Open, FileAccess.Read);
+    using StreamReader leitor = new StreamReader(fs);
+    string? linha;
+    while((linha = leitor.ReadLine()) != null)
+    {
+        Console.WriteLine(linha);
+    }
+
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.ReadKey();
